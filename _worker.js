4ce93d09e2f663af8711be55bdfca6a59e462e19
2525255 <94049432+2525255@users.noreply.github.com>
2026-08-04 15:42:33 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -288,7 +288,7 @@ export default {
         const pwdMask = document.getElementById('pwdMask');
         const pwdInput = document.getElementById('pwdInput');
         let waitingCopyId = "";
-        const correctPwd = "4536";
+        const correctPwd = "8520";
 
         function showTip(msg){
           tipText.innerText = msg;
