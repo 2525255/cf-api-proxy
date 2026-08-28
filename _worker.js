dfd79a29913ed57fa1a75f1f71d0f0ea8ca2ed50
2525255 <94049432+2525255@users.noreply.github.com>
2026-08-28 11:45:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -396,7 +396,7 @@ export default {
 
       <script>
         // ========== 今日数据码 ==========
-        const DATA_CODE = "8787";
+        const DATA_CODE = "9896";
         const codeMask = document.getElementById('codeMask');
         const codeInput = document.getElementById('codeInput');
         const codeError = document.getElementById('codeError');
@@ -427,7 +427,7 @@ export default {
         const pwdMask = document.getElementById('pwdMask');
         const pwdInput = document.getElementById('pwdInput');
         let waitingCopyId = "";
-        const correctPwd = "85232";
+        const correctPwd = "741852";
 
         function showTip(msg){
           tipText.innerText = msg;
