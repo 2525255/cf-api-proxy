68cbd9b000c22940f9c408562c7751180b0ab557
2525255 <94049432+2525255@users.noreply.github.com>
2026-08-30 18:33:42 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -396,7 +396,7 @@ export default {
 
       <script>
         // ========== 今日数据码 ==========
-        const DATA_CODE = "7866";
+        const DATA_CODE = "7866";  // 数据码修改入口
         const codeMask = document.getElementById('codeMask');
         const codeInput = document.getElementById('codeInput');
         const codeError = document.getElementById('codeError');
@@ -427,7 +427,7 @@ export default {
         const pwdMask = document.getElementById('pwdMask');
         const pwdInput = document.getElementById('pwdInput');
         let waitingCopyId = "";
-        const correctPwd = "789456";
+        const correctPwd = "789456"; // 压缩包及口令密钥修改入口
 
         function showTip(msg){
           tipText.innerText = msg;
