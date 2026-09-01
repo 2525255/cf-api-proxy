50fed650962fed260d8dde752878fb46d4496f3b
2525255 <94049432+2525255@users.noreply.github.com>
2026-09-01 11:03:05 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -396,7 +396,7 @@ export default {
 
       <script>
         // ========== 今日数据码 ==========
-        const DATA_CODE = "7866";  // 数据码修改入口
+        const DATA_CODE = "2256";  // 数据码修改入口
         const codeMask = document.getElementById('codeMask');
         const codeInput = document.getElementById('codeInput');
         const codeError = document.getElementById('codeError');
