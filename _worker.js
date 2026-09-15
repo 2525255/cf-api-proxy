1818acb2ba2d70540adf43af655bbbb3ae422176
2525255 <94049432+2525255@users.noreply.github.com>
2026-09-15 10:12:46 +0800

    Change correct password in _worker.js
    Updated the password from '852852' to '788797'.

--- _worker.js
+++ _worker.js
@@ -427,7 +427,7 @@ export default {
         const pwdMask = document.getElementById('pwdMask');
         const pwdInput = document.getElementById('pwdInput');
         let waitingCopyId = "";
-        const correctPwd = "852852"; // 压缩包及口令密钥修改入口
+        const correctPwd = "788797"; // 压缩包及口令密钥修改入口
 
         function showTip(msg){
           tipText.innerText = msg;
